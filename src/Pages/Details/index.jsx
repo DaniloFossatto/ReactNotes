ba94447aa8a50ecd7a4793fdaac082ee262bfec0
Danilo <danilofossatto2011@hotmail.com>
2024-02-17 21:45:48 -0300

    added global styles

---
+++ src/Pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import {Container} from './styles'
+
+export default function Details(){
+
+    return(
+        <Container>
+            <h1>OPA</h1>
+        </Container>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import {ThemeProvider} from "styled-components"
+import Details from './Pages/Details'
+import  theme from './styles/theme'
+import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={ theme }>
+    <GlobalStyles/>
+    <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
