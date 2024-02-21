1706149f92cc5a1fdf4c4a2d1bf381081a08e033
Danilo <danilofossatto2011@hotmail.com>
2024-02-21 15:40:14 -0300

    Working with Images

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,8 +2,10 @@ import {Container, Brand, Menu, Search, Content, NewNote} from './styles'
 
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 import {FiPlus} from 'react-icons/fi'
+import { Note } from '../../components/Note'
 
 export function Home(){
     
@@ -26,7 +28,13 @@ export function Home(){
         </Search>
 
         <Content>
-
+        <Section title="Minhas notas">
+        <Note data={{title: 'react', tags: [
+            {id: 1, name: 'react'},
+            {id: 2, name: 'node'}
+            ]
+            }}/>
+        </Section>
         </Content>
 
         <NewNote>
