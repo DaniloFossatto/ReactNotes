9a6cd2226554d39ecfae78607a0692adc23ed43d
Danilo <danilofossatto2011@hotmail.com>
2024-02-21 23:00:00 -0300

    added new note

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from "styled-components"
-import {Profile} from './Pages/Profile'
+import { New } from './Pages/New'
 import  theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
@@ -9,7 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={ theme }>
     <GlobalStyles/>
-    <Profile />
+    <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
+ 
